b41a370879cf1de0de450891cf6b81398b20b96f
Brian Faust <hello@brianfaust.de>
2017-07-23 05:20:28 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Models/Message.php
+++ src/Models/Message.php
@@ -11,12 +11,12 @@
 
 namespace BrianFaust\Messageable\Models;
 
+use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Message extends Model
 {
